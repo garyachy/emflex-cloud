9bcf8eabfb1e2cd34fc881b30b125c5fdebaa693
Denys Haryachyy <garyachy@gmail.com>
2017-01-25 23:21:58 +0200

    Initialize gmap in a relative position

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,7 +5,6 @@ MAPAPP.currentInfoWindow;
 MAPAPP.pathName = window.location.pathname;
 
 $(document).ready(function() {
-    initialize();
     populateMarkers(MAPAPP.pathName);
 	displayTime();
 });
@@ -21,8 +20,8 @@ function displayTime() {
 };
 
 //Initialize our Google Map
-function initialize() {
-    var center = new google.maps.LatLng(39.9543926,-75.1627432);
+function initialize(lat,lng) {
+    var center = new google.maps.LatLng(lat,lng);
     var mapOptions = {
         zoom: 13,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -39,27 +38,35 @@ function populateMarkers(dataType) {
     $.getJSON(apiLoc, function(data) {
         //For each item in our JSON, add a new map marker
         $.each(data, function(i, ob) {
-            var marker = new google.maps.Marker({
-                map: map,
-                position: new google.maps.LatLng(this.location.coordinates[0], this.location.coordinates[1]),
-                shopname: this.shopname,
-                details: this.details,
-                website: this.website,
-                icon: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png'
-            });
-    	//Build the content for InfoWindow
-            var content = '<h1 class="mt0"><a href="' + marker.website + '" target="_blank" title="' + marker.shopname + '">' + marker.shopname + '</a></h1><p>' + marker.details + '</p>';
-        	marker.infowindow = new google.maps.InfoWindow({
-            	content: content,
-            	maxWidth: 400
-            });
-    	//Add InfoWindow
-            google.maps.event.addListener(marker, 'click', function() {
-                if (MAPAPP.currentInfoWindow) MAPAPP.currentInfoWindow.close();
-                marker.infowindow.open(map, marker);
-                MAPAPP.currentInfoWindow = marker.infowindow;
-            });
-            MAPAPP.markers.push(marker);
+			if (i == 0) {
+				initialize(ob.location.coordinates[0], ob.location.coordinates[1]);
+			};
+            createMarker (ob);
         });
     });
+};
+
+//Create marker
+function createMarker(shop) {
+    var marker = new google.maps.Marker({
+        map: map,
+        position: new google.maps.LatLng(shop.location.coordinates[0], shop.location.coordinates[1]),
+        shopname: shop.shopname,
+        details: shop.details,
+        website: shop.website,
+        icon: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png'
+    });
+//Build the content for InfoWindow
+    var content = '<h1 class="mt0"><a href="' + marker.website + '" target="_blank" title="' + marker.shopname + '">' + marker.shopname + '</a></h1><p>' + marker.details + '</p>';
+	marker.infowindow = new google.maps.InfoWindow({
+    	content: content,
+    	maxWidth: 400
+    });
+//Add InfoWindow
+    google.maps.event.addListener(marker, 'click', function() {
+        if (MAPAPP.currentInfoWindow) MAPAPP.currentInfoWindow.close();
+        marker.infowindow.open(map, marker);
+        MAPAPP.currentInfoWindow = marker.infowindow;
+    });
+    MAPAPP.markers.push(marker);
 };
